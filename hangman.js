384a6f620388edd20dca0b8bd6c370f0a87edf7a
Ania <anna.polgert@gmail.com>
2020-12-02 14:42:42 +0100

    added showTheLetter function

--- hangman.js
+++ hangman.js
@@ -1,7 +1,8 @@
-var password = "Dla chcącego nic trudnego";
+var password = "DLA CHCĄCEGO NIC TRUDNEGO";
 var board = document.querySelector(".board");
 var lettersNumber = password.length;
 var hiddenPassword = "";
+var letters = document.querySelectorAll(".letter")
 
 for (i=0; i<lettersNumber; i++) {
    if(password.charAt(i) === " ") {
@@ -16,3 +17,23 @@ var showPassword = function() {
 }
 
 showPassword();
+
+String.prototype.showTheLetter = function(position, character) {
+	if (position > this.length - 1) {
+      return this.toString();
+   } else {
+      return this.substr(0, position) + character + this.substr(position + 1);
+   }
+};
+
+letters.forEach(function(btn) {
+   btn.addEventListener("click", function(event) {
+      for (i=0; i<lettersNumber; i++) {
+         if(password.charAt(i) === event.target.innerText) {
+            console.log(hiddenPassword.showTheLetter(i,event.target.innerText));
+            hiddenPassword = hiddenPassword.showTheLetter(i,event.target.innerText)
+            showPassword();
+         }
+      }   
+   })
+});
